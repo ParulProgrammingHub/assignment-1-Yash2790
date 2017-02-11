c066959ae9263f999198a384a02b155c418e626b
Yash2790 <yashde2790@gmail.com>
2017-02-11 23:41:41 +0530

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,8 @@
+pri = float(input(" Enter the principal "))
+rate = float(input(" Enter the interest rate "))
+years= float(input(" Enter the number of years "))
+def compound_interest(pri,rate,years):
+    if years<=0:
+          return pri
+    else:  return compound_interest(pri+pri*rate/100,rate,years-1)
+print(" Compound is ", compound_interest(pri,rate,years))
